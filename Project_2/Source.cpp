bc99148be4da8b51fb3542e84aef306b85018046
LTimo <tlabsky@gmail.com>
2017-11-20 12:51:16 +0100

    repaired of fnc N and V working on fnc P

--- Project_2/Source.cpp
+++ Project_2/Source.cpp
@@ -30,6 +30,7 @@ void	fnc_p(AUTA **auta_first, int number_of_records);
 char*	safe_copy_string_form_file(FILE *f);
 int		safe_copy_int_from_file(FILE *f);
 void	alloc_auta(AUTA* auta_alloc);
+void	fnc_free(AUTA **auta_first);
 
 
 
@@ -43,17 +44,20 @@ void main() {
 
 	while (function_char != 'k') {
 		scanf("%c", &function_char);
-		
+
 		switch (function_char)
 		{
 		case 'n':
-			number_of_records = fnc_n(&auta_first);	
+			number_of_records = fnc_n(&auta_first);
 			break;
 		case 'v':
 			fnc_v(auta_first);
+			//printf("%s", auta_first->dalsi->dalsi->dalsi->kategoria);
 			break;
 		case 'p':
-			fnc_p(&auta_first,number_of_records);
+			fnc_p(&auta_first, number_of_records);
+		case 'f':
+			fnc_free(&auta_first);
 		default:
 			break;
 		}
@@ -65,7 +69,7 @@ void main() {
 
 int fnc_n(AUTA **auta_first) {
 	FILE *file_to_read;
-	AUTA *auta_act;
+	AUTA *auta_act, *temp = NULL;
 	int type_of_record, number_of_records;
 	char ch;
 
@@ -78,57 +82,61 @@ int fnc_n(AUTA **auta_first) {
 		return 0;
 	}
 
+	//dealoc 
+	while (*auta_first != NULL)
+	{
+		temp = *auta_first;
+		*auta_first = (*auta_first)->dalsi;
+		free(temp);
+
+	}
+	
+
 	//inicial allocation of struct + error handling
 	auta_act = (AUTA *)malloc(sizeof(AUTA));
 	if (auta_act == NULL) {
 		printf("nedostatok pamati\n");
+		return 0;
 	}
+	alloc_auta(auta_act);
 	auta_act->dalsi = NULL;
 	*auta_first = auta_act;
-
 
 
 	while (!feof(file_to_read)) {
 		//recognizing start and counting of records
 		if ((ch = fgetc(file_to_read)) == '$') {
 			number_of_records++;
-		}
-		else if (ch == '\0') {
-			break;
-		}
-		else
-		{
-			alloc_auta(auta_act);
 
-			//reading from file and saving it to linked list
-			auta_act->kategoria = safe_copy_string_form_file(file_to_read);
-			auta_act->znacka = safe_copy_string_form_file(file_to_read);
-			auta_act->predajca = safe_copy_string_form_file(file_to_read);
-			auta_act->cena = safe_copy_int_from_file(file_to_read);
-			auta_act->rok_vyroby = safe_copy_int_from_file(file_to_read);
-			auta_act->stav_vozidla = safe_copy_string_form_file(file_to_read);
-
-			if (number_of_records == 1) {
-				auta_act->dalsi = (auta*)malloc(sizeof(auta));
-				alloc_auta(auta_act->dalsi);
-				auta_act = auta_act->dalsi;
-				auta_act->dalsi = NULL;
-		
+			if (ch == '\0') {
+				break;
 			}
 			else
 			{
-				auta_act->dalsi = (auta*)malloc(sizeof(auta));
-				alloc_auta(auta_act->dalsi);
-				auta_act = auta_act->dalsi;
-				auta_act->dalsi = NULL;
+				if (number_of_records > 1) {
+					auta_act->dalsi = (auta*)malloc(sizeof(auta));
+					alloc_auta(auta_act->dalsi);
+					auta_act = auta_act->dalsi;
+					auta_act->dalsi = NULL;
+				}
+				//reading from file and saving it to linked list
+				ch = fgetc(file_to_read);
+				auta_act->kategoria = safe_copy_string_form_file(file_to_read);
+				auta_act->znacka = safe_copy_string_form_file(file_to_read);
+				auta_act->predajca = safe_copy_string_form_file(file_to_read);
+				auta_act->cena = safe_copy_int_from_file(file_to_read);
+				auta_act->rok_vyroby = safe_copy_int_from_file(file_to_read);
+				auta_act->stav_vozidla = safe_copy_string_form_file(file_to_read);
+				
 			}
 		}
 	}
-	printf("Nacitalo sa %d zaznamov\n", number_of_records);
-	free(auta_act->dalsi);
-	fclose(file_to_read);
-	return number_of_records;
-}
+		printf("Nacitalo sa %d zaznamov\n", number_of_records);
+		free(auta_act->dalsi);
+		fclose(file_to_read);
+		return number_of_records;
+	}
+
 
 void fnc_v(AUTA *auta_first) {
 	AUTA* auta_print = auta_first;
@@ -138,44 +146,66 @@ void fnc_v(AUTA *auta_first) {
 		return;
 	}
 
-	while (auta_print->dalsi != NULL) {
+	while (auta_print != NULL) {
 		printf("%d.\n", ++number_of_elements);
-		printf("kategoria: %s\n", auta_print->kategoria);
-		//printf("zancka: %s\n", auta_print->znacka);
-		//printf("predajca: %s\n", auta_print->predajca);
-		//printf("cena: %d\n", auta_print->cena);
-		//printf("rok_vyroby: %d\n", auta_print->rok_vyroby);
-		//printf("stav_vozidla: %s\n", auta_print->stav_vozidla);
+		printf("kategoria: %s", auta_print->kategoria);
+		printf("zancka: %s", auta_print->znacka);
+		printf("predajca: %s", auta_print->predajca);
+		printf("cena: %d\n", auta_print->cena);
+		printf("rok_vyroby: %d\n", auta_print->rok_vyroby);
+		printf("stav_vozidla: %s", auta_print->stav_vozidla);
 		auta_print = auta_print->dalsi;
 	}
 
 }
 
 void fnc_p(AUTA **auta_first, int number_of_records) {
-	AUTA *auta_copy, *auta_temp;
-	int position, act_position; 
+	AUTA *auta_act, *auta_to_add;
+	int position_to_add, position_actual;
 
-	auta_copy = *auta_first;
-	auta_temp = NULL;
-	scanf("%d", &position);
-
-	act_position = 0;
-
-	while (auta_copy->dalsi != NULL) {
-		act_position++;
-		auta_temp = (AUTA *)malloc(sizeof(AUTA));
-		alloc_auta(auta_temp);
-		auta_temp->dalsi = NULL;
-		if (act_position == position) {
-			auta_temp->kategoria = "treba to spravit";
-		}
-		else {
-			auta_temp = auta_copy;
-			auta_copy = auta_copy->dalsi;
-		}
+	auta_to_add = NULL;
+	scanf("%d\n", &position_to_add);
+	auta_act = *auta_first;
+	position_actual = 0;
+	
+	if (auta_to_add == NULL) {
+		auta_to_add = (AUTA *)malloc(sizeof(AUTA));
+		alloc_auta(auta_to_add);
+		auta_to_add->dalsi = NULL;
 	}
 
+	//load of data to be writen
+	fgets((auta_to_add->kategoria), kategoria_size, stdin);
+	fgets((auta_to_add->znacka), znacka_size, stdin);
+	fgets((auta_to_add->predajca), predajca_size, stdin);
+	scanf("%d\n", &auta_to_add->cena);
+	scanf("%d\n", &auta_to_add->rok_vyroby);
+	fgets((auta_to_add->stav_vozidla), stav_vozidla_size, stdin);
+
+	while (auta_act != NULL) {
+		position_actual++;
+		if (position_actual = position_to_add-1) {
+			if (auta_act->dalsi == NULL) {
+				auta_act->dalsi = (AUTA *)malloc(sizeof(AUTA));
+				alloc_auta(auta_act->dalsi);
+			}
+			auta_act->dalsi = auta_to_add;
+			auta_act = auta_act->dalsi;
+			auta_to_add->dalsi = auta_act;
+			
+		}
+		else if (position_actual > position_to_add)
+		{
+
+		}
+		else
+		{
+			auta_act = auta_act->dalsi;
+		}
+	
+	}
 }
+
 
 char*	safe_copy_string_form_file(FILE *f) {
 		/*
@@ -198,7 +228,8 @@ char*	safe_copy_string_form_file(FILE *f) {
 		for (i = 0; i < 50; i++) {
 			buffer_char = fgetc(f);
 			if ((buffer_char == '\n') || (feof(f))) {
-				buffer_string[i] = '\0';
+				buffer_string[i] = '\n';
+				buffer_string[i + 1] = '\0';
 				break;
 			}
 			buffer_string[i] = buffer_char;
@@ -210,11 +241,11 @@ char*	safe_copy_string_form_file(FILE *f) {
 int	safe_copy_int_from_file(FILE *f) {
 	char integer_str[11];
 	int integer;
-
 	for (int i = 0; i < 11; i++) {
 		integer_str[i] = fgetc(f);
 		if (integer_str[i] == '\n') {
-			integer_str[i] = '\0';
+			integer_str[i] = '\n';
+			integer_str[i + 1] = '\0';
 			break;
 		}
 	}
@@ -224,8 +255,28 @@ int	safe_copy_int_from_file(FILE *f) {
 }
 
 void   alloc_auta(AUTA* auta_act) {
+	if (auta_act == NULL) {
+		printf("error alloc auta\n");
+		return;
+	}
 	auta_act->kategoria = (char*)malloc(kategoria_size * sizeof(char));
 	auta_act->znacka = (char*)malloc(znacka_size * sizeof(char));
 	auta_act->predajca = (char*)malloc(predajca_size * sizeof(char));
 	auta_act->stav_vozidla = (char*)malloc(stav_vozidla_size * sizeof(char));
+}
+
+void	fnc_free(AUTA **auta_first) { 
+	/*
+	 *   test of deallocation
+	*/
+	
+	AUTA *temp;
+
+	while (*auta_first != NULL)
+	{
+		temp = *auta_first;
+		*auta_first = (*auta_first)->dalsi;
+		free(temp);
+
+	}
 }
